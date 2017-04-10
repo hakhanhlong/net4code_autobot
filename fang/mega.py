2152b7faa2ed7eec3ce26c5706f2ca39944a6a9c
longhk <hakhanhlong@gmail.com>
2017-04-10 06:20:33 -0700

    m

--- fang/mega.py
+++ fang/mega.py
@@ -160,8 +160,9 @@ class Mega(threading.Thread):
                                 end_by = '\r\n'
                             compare_value = stringhelpers.find_between(cmd_log['console_log'], start_by, end_by)
                             if compare_value is not None or compare_value is not '':
-                                compare_value = int(compare_value)
-                                retvalue_compare = self.func_compare(compare, standard_value, compare_value)
+                                if compare != "contains":
+                                    compare_value = int(compare_value)
+                                retvalue_compare = self.func_compare(compare, standard_value, compare_value.strip())
                                 output_result.append({'value': compare_value, 'compare': retvalue_compare})
                                 # save final result of each output
                                 final_result_output.append(retvalue_compare)
@@ -169,6 +170,7 @@ class Mega(threading.Thread):
                             _strError = "MEGA PARSING COMMAND TYPE %d ERROR %s | THREAD %s" % (command_type, _error, self.name)
                             stringhelpers.err(_strError)
                             output_result.append({'value': compare_value, 'compare': retvalue_compare, 'error': _strError})
+                            cmd_log['parsing_status'] = 'ERROR'
 
 
                 # determine operator for final output
@@ -193,7 +195,8 @@ class Mega(threading.Thread):
                 cmd_log['result']['outputs'] = output_result
                 return cmd_log
         except Exception as _errorException:
-            pass
+            cmd_log['parsing_status'] = 'ERROR'
+            return  cmd_log
 
 
 
@@ -207,26 +210,29 @@ class Mega(threading.Thread):
             '<': self.compare_lessthan,
             '<>': self.compare_notequal,
             '&': self.compare_final_equal,
-            '|': self.compare_final_or
+            '|': self.compare_final_or,
+            'contains': self.compare_contants
         }
         func = compare_operator.get(argument)
         return func(standard_value, compare_value)
 
-    def compare_lessorequal(self, standard_value = 0, compare_value = 0):
+    def compare_contants(self, standard_value = None, compare_value = None): #compare check string in
+        return standard_value in compare_value
+    def compare_lessorequal(self, standard_value = None, compare_value = None): #compare int
         return compare_value <= standard_value
-    def compare_largerorequal(self, standard_value = 0, compare_value = 0):
+    def compare_largerorequal(self, standard_value = None, compare_value = None): #compare int
         return compare_value >= standard_value
-    def compare_equal(self, standard_value = 0, compare_value = 0):
+    def compare_equal(self, standard_value = None, compare_value = None): #compare int
         return compare_value == standard_value
-    def compare_larger(self, standard_value = 0, compare_value = 0):
+    def compare_larger(self, standard_value = None, compare_value = None): #compare int
         return compare_value > standard_value
-    def compare_lessthan(self, standard_value = 0, compare_value = 0):
+    def compare_lessthan(self, standard_value = None, compare_value = None): #compare int
         return compare_value < standard_value
-    def compare_notequal(self, standard_value = 0, compare_value = 0):
+    def compare_notequal(self, standard_value = None, compare_value = None): #compare int
         return compare_value != standard_value
-    def compare_final_equal(self, output_value = 0, output_value_ = 0):
+    def compare_final_equal(self, output_value = None, output_value_ = None): #compare true or false
         return output_value == output_value_
-    def compare_final_or(self, output_value = 0, output_value_ = 0):
+    def compare_final_or(self, output_value = None, output_value_ = None): #compare true or false
         return output_value == output_value_
 
 
