c3ad70bc37e692b37d28967ae70b3ada481785c4
longhk <hakhanhlong@gmail.com>
2017-05-11 01:40:59 -0700

    discovery

--- database/impl/lldp_impl.py
+++ database/impl/lldp_impl.py
@@ -15,6 +15,7 @@ class LLDPImpl():
         remote_interface = kwargs['remote_interface']
         local_interface = kwargs['local_interface']
         remote_device = kwargs['remote_device']
+        #remote_interface_id = kwargs['remote_interface_id']
         s = LLDP(interface_id=interface_id, remote_interface=remote_interface,
                  local_interface=local_interface, remote_device=remote_device,
                  data=kwargs['data'])
@@ -26,11 +27,13 @@ class LLDPImpl():
         remote_interface = kwargs['remote_interface']
         local_interface = kwargs['local_interface']
         remote_device = kwargs['remote_device']
+        remote_interface_id = kwargs['remote_interface_id']
         s = LLDP.objects(interface_id=interface_id).first()
         s.data = kwargs['data']
         s.remote_interface = remote_interface
         s.local_interface = local_interface
         s.remote_device = remote_device
+        #s.remote_interface_id = remote_interface_id
         s.modified = datetime.datetime.now()
         return s.save()
 
